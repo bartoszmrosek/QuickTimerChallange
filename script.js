aa7d707733c69db8d24d8814ef48eb1faad381c8
Bartosz Mrosek <73000377+bartoszmrosek@users.noreply.github.com>
2022-11-29 11:58:19 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+storedRecords = [];
+
+const setRecord = timer => {
+  storedRecords.push({id: storedRecords.length + 1, record: timer});
+};
+
+const retrieveRecords = () => {
+  const archiveDisplay = document.getElementsByClassName('stoper-archive')[0];
+  archiveDisplay.innerHTML ="";
+    for(records of storedRecords){
+        const newParagraph = document.createElement('p');
+        newParagraph.innerText = `${records.id} ${record.record}`
+        archiveDisplay.appendChild(newParagraph);
+    }
+};
