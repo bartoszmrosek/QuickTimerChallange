acce48c7059f695684e3b97d5d7e918329695708
Bartosz Mrosek <73000377+bartoszmrosek@users.noreply.github.com>
2022-11-29 12:50:15 +0100

    Finished product

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-storedRecords = [];
+let storedRecords = [];
+let stopper = document.getElementById('stoper');
+const archiveDisplay = document.getElementsByClassName('stoper-archive')[0];
+let timer;
+let isStarted = false;
+let seconds = 0;
+let minutes = 0;
+const decide = (start = false) => {
+  if (start === true && isStarted === false) {
+    timer = setInterval(() => {
+      seconds++;
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+      stopper.innerText = `${minutes}:${seconds}`;
+    }, 1000);
+    isStarted = true;
+  }
+  if (!isStarted) {
+    clearInterval(timer);
+    isStarted = false;
+  }
+};
 
 const setRecord = () => {
-    const timer = document.getElementById('stoper').innerText;
-  storedRecords.push({id: storedRecords.length + 1, record: timer});
+  decide(false);
+  const stopper = document.getElementById('stoper').innerText;
+  storedRecords.push({id: storedRecords.length + 1, record: stopper});
+};
+
+const stopTimer = () => {
+  clearInterval(timer);
+  isStarted = false;
 };
 
 const retrieveRecords = () => {
-  const archiveDisplay = document.getElementsByClassName('stoper-archive')[0];
-  archiveDisplay.innerHTML ="";
-    for(records of storedRecords){
-        const newParagraph = document.createElement('p');
-        newParagraph.innerText = `${records.id} ${records.record}`
-        archiveDisplay.appendChild(newParagraph);
-    }
+  archiveDisplay.innerHTML = '';
+  setRecord();
+  for (records of storedRecords) {
+    const newParagraph = document.createElement('p');
+    newParagraph.innerText = `${records.id} ${records.record}`;
+    archiveDisplay.appendChild(newParagraph);
+  }
+};
+
+const clearRecords = () => {
+  archiveDisplay.innerHTML = '';
+  storedRecords = [];
+  stopper.innerText = '0:0';
+  seconds = 0;
+  minutes = 0;
+  isStarted = false;
+  clearInterval(timer);
 };
